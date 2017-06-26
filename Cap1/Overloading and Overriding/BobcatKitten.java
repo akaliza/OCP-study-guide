e2e7a84de29ab912ba8a30f6a76febae8290a78d
Rafael <rafael_possenti@hotmail.com>
2017-06-26 17:20:32 -0300

    add the overload and overwrite examples

---
+++ Cap1/Overloading and Overriding/BobcatKitten.java
@@ -0,0 +1,5 @@
+public class BobcatKitten extends Bobcat {
+	public void findDen() { } /*overwrite*/
+	public void findDen(boolean b) { } /*overload*/
+	public int findden() throws Exception { return 0; } /*the name is not the same*/ 
+}
