f0ef3b2e5c68fa681d121cc9225cade419be6a45
Rafael <rafael_possenti@hotmail.com>
2017-06-28 13:26:07 -0300

    add Enums example

---
+++ Cap1/Enums/Main.java
@@ -0,0 +1,34 @@
+
+public class Main{
+	public static void main(String[] args){
+		/* Season[OLD]
+		Season s = Season.SUMMER;
+		System.out.println(Season.SUMMER); // SUMMER
+		System.out.println(s == Season.SUMMER); // true
+		
+		for(Season season: Season.values()) {
+			System.out.println(season.name() + " " + season.ordinal());
+		}
+		
+		Season s1 = Season.valueOf("SUMMER"); // SUMMER
+		//Season s2 = Season.valueOf("summer"); // exception
+		
+		Season summer = Season.SUMMER;
+		switch (summer) {
+			case WINTER:
+				System.out.println("Get out the sled!");
+				break;
+			case SUMMER:
+				System.out.println("Time for the pool!");
+				break;
+			default:
+				System.out.println("Is it summer yet?");
+		}
+		*/
+		
+		Season2 s = Season2.SUMMER;
+		System.out.println(Season2.SUMMER); // SUMMER
+	
+	}
+	
+}
---
+++ Cap1/Enums/OnlyOne.java
@@ -0,0 +1,10 @@
+public enum OnlyOne {
+	ONCE (true);
+	private OnlyOne(boolean b) {
+		System.out.println("constructing");
+	}
+	public static void main(String[] args) {
+		OnlyOne firstCall = OnlyOne.ONCE ; // prints constructing
+		OnlyOne secondCall = OnlyOne.ONCE ; // doesn't print anything
+	} 
+}
