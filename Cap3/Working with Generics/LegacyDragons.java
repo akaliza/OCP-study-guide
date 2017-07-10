b0ca3962f7ab6bca48231f33d2616f8650e48033
Rafael Possenti <rafael_possenti@hotmail.com>
2017-07-09 23:27:19 -0300

    LegacyDragons.java

---
+++ Cap3/Working with Generics/LegacyDragons.java
@@ -0,0 +1,20 @@
+import java.util.*; 
+
+class Dragon {}
+
+class Unicorn { }
+
+public class LegacyDragons {
+
+	public static void main(String[] args) {
+		List unicorns = new ArrayList();
+		unicorns.add(new Unicorn());
+		printDragons(unicorns);
+	}
+
+	private static void printDragons(List<Dragon> dragons) {
+		for (Dragon dragon: dragons) { // ClassCastException
+			System.out.println(dragon);
+		} 
+	} 
+}
