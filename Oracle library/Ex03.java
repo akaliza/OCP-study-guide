35fea4b3e6e18e34b78116365d5b13a07651be61
Rafael Possenti <rafael_possenti@hotmail.com>
2017-07-15 20:42:07 -0300

    Exe04

---
+++ Oracle library/Ex03.java
@@ -0,0 +1,23 @@
+import java.util.*; 
+import java.util.function.Predicate;
+
+public class Ex03{
+	public static void main(String... args){
+		
+		Student s1 = new Student("A",2011,95);
+		Student s2 = new Student("B",2011,98);
+		Student s3 = new Student("C",2011,82);
+		
+		
+		List<Student> students = new ArrayList<>(); 
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		
+		double highestScore =  students.filter((Student s) -> s.getGradYear() == 2011).map((Student s) -> s.getScore()).max();
+
+		
+		System.out.println("Highest grade is " + highestScore);
+	}
+	
+}
---
+++ Oracle library/Ex04.java
@@ -0,0 +1,11 @@
+import java.util.*; 
+import java.util.function.Predicate;
+
+public class Ex04{
+	public static void main(String... args){
+		
+		
+		Factory<List<String>> f = ArrayList<String>::new;
+	}
+	
+}
