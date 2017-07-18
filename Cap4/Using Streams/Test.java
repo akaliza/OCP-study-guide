5481d331396a00cdf2dab092ed0501bb1f273a76
Rafael <rafael_possenti@hotmail.com>
2017-07-18 13:38:26 -0300

    Stream example

--- Cap4/Using Streams/Test.java
+++ Cap4/Using Streams/Test.java
@@ -2,8 +2,16 @@ import java.util.*;
 
 public class Test{
 	public static void main(String... args){
+		/*
 		List<String> list = Arrays.asList("a", "b", "c");
 		Stream<String> fromList = list.stream();
 		Stream<String> fromListParallel = list.parallelStream();
+		*/
+		
+		List<String> list = Arrays.asList("Toby", "Anna", "Leroy", "Alex");
+		list.stream().filter(n -> n.length() == 4)
+			.sorted()
+			.limit(2)
+			.forEach(System.out::println);
 	}
 }
