ec9b825ddf7f2089cc630d6fd21b4ecf30ddceea
Rafael <rafael_possenti@hotmail.com>
2017-07-19 16:55:17 -0300

    Period Example

---
+++ Cap5/Working with Dates and Times/Periodo.java
@@ -0,0 +1,19 @@
+import java.time.*;
+
+public class Periodo{
+	public static void main(String[] args) {
+		LocalDate start = LocalDate.of(2015, Month.JANUARY, 1);
+		LocalDate end = LocalDate.of(2015, Month.MARCH, 30);
+		Period period = Period.ofMonths(1); // create a period
+		performAnimalEnrichment(start, end, period);
+	}
+
+	private static void performAnimalEnrichment(LocalDate start, LocalDate end, Period period) { // uses the generic period
+		LocalDate upTo = start;
+		while (upTo.isBefore(end)) {
+			System.out.println("give new toy: " + upTo);
+			upTo = upTo.plus(period); // adds the period
+		}
+
+	}
+}
