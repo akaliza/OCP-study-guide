7d9c1d70fe78db5cc9bbe720f778d29da7492a48
Rafael <rafael_possenti@hotmail.com>
2017-07-21 06:46:39 -0300

    AutoCloseable example

---
+++ Cap6/Using Try With Resources/TurkeyCage.java
@@ -0,0 +1,12 @@
+public class TurkeyCage implements AutoCloseable {
+	
+	public void close() {
+		System.out.println("Close gate");
+	}
+	
+	public static void main(String[] args) {
+		try (TurkeyCage t = new TurkeyCage()) {
+			System.out.println("put turkeys in");
+		}
+	}
+}
