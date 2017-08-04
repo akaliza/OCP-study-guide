8a6bc193ad8e563c78f5c7c54b25bc4d8efeb762
Rafael <rafael_possenti@hotmail.com>
2017-08-04 12:37:10 -0300

    Unbounded

--- Cap3/Working with Generics/Unbounded.java
+++ Cap3/Working with Generics/Unbounded.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Unbounded{
 
-	public static void printList(List<?> list) {
+	public static void printList(List<?> list) { /*List<Object> list*/
 		for (Object x: list) System.out.println(x);
 	}
 
 	public static void main(String[] args) {
 		List<String> keywords = new ArrayList<>();
 		keywords.add("java");
-		printList(keywords);
+		printList(keywords); 
 	}
 	
 }
