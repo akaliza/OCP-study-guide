44e8f83f949abd880b57a396a05b1a32a97d3c90
Rafael <rafael_possenti@hotmail.com>
2017-08-15 13:35:50 -0300

    Cap4

--- Cap4/Working with Advanced Stream/AdvancedStream.java
+++ Cap4/Working with Advanced Stream/AdvancedStream.java
@@ -2,6 +2,9 @@ import java.util.stream.*;
 import java.util.*;
 import java.util.function.*;
 
+
+
+
 public class AdvancedStream{
 		
 		private static void lazilyStream() {
@@ -11,6 +14,7 @@ public class AdvancedStream{
 			Stream<String> stream = cats.stream();
 			cats.add("KC");
 			System.out.println(stream.count());
+			System.out.println();
 		}
 		
 		private static void threeDigit(Optional<Integer> optional) {
@@ -28,11 +32,94 @@ public class AdvancedStream{
 				.ifPresent(System.out::println); // part 3
 		}
 		
+		private static void collecting(){
+			Stream<String> ohMy = Stream.of("lions", "tigers", "bears");
+			String result = ohMy.collect(Collectors.joining(", "));
+			System.out.println(result); // lions, tigers, bears
+			
+			Stream<String> ohMy2 = Stream.of("lions", "tigers", "bears");
+			Double result2 = ohMy2.collect(Collectors.averagingInt(String::length));
+			System.out.println(result2); // 5.333333333333333
+			
+			/*
+			Stream<String> ohMy3 = Stream.of("lions", "tigers", "bears");
+			TreeSet<String> result3 = ohMy3.filter(s -> s.startsWith("t").collect(Collectors.toCollection(TreeSet::new)));
+			System.out.println(result3); // [tigers]
+			*/
+			System.out.println();
+		}
+		
+		private static void collectingIntoMaps(){
+			Stream<String> ohMy = Stream.of("lions", "tigers", "bears");
+			Map<String, Integer> map = ohMy.collect(
+				Collectors.toMap(s -> s, String::length));
+			System.out.println(map); // {lions=5, bears=5, tigers=6}
+			
+			Stream<String> ohMy2 = Stream.of("lions", "tigers", "bears");
+			Map<Integer, String> map2 = ohMy2.collect(Collectors.toMap(
+			String::length, k -> k, (s1, s2) -> s1 + "," + s2));
+			System.out.println(map2); // {5=lions,bears, 6=tigers}
+			System.out.println(map2.getClass()); // class. java.util.HashMap
 
+			System.out.println();
+		}
+
+		
+		private static void collectingUsingGPM(){
+			Stream<String> ohMy = Stream.of("lions", "tigers", "bears", "bee");
+			Map<Integer, List<String>> map = ohMy.collect(
+			Collectors.groupingBy(String::length));
+			System.out.println(map); // {3=[bee],5=[lions, bears], 6=[tigers]}
+
+			Stream<String> ohMy2 = Stream.of("lions", "tigers", "bears");
+			Map<Integer, Set<String>> map2 = ohMy2.collect(
+			Collectors.groupingBy(String::length, Collectors.toSet()));
+			System.out.println(map2); // {5=[lions, bears], 6=[tigers]}
+			
+			Stream<String> ohMy3 = Stream.of("lions", "tigers", "bears");
+			TreeMap<Integer, Set<String>> map3 = ohMy3.collect(
+			Collectors.groupingBy(String::length, TreeMap::new, Collectors.toSet()));
+			System.out.println(map3); // {5=[lions, bears], 6=[tigers]}
+			
+			Stream<String> ohMy4 = Stream.of("lions", "tigers", "bears");
+			Map<Boolean, List<String>> map4 = ohMy4.collect(
+			Collectors.partitioningBy(s -> s.length() <= 5));
+			System.out.println(map4); // {false=[tigers], true=[lions, bears]}
+
+			Stream<String> ohMy5 = Stream.of("lions", "tigers", "bears");
+			Map<Integer, Long> map5 = ohMy5.collect(Collectors.groupingBy(
+			String::length, Collectors.counting()));
+			System.out.println(map5); // {5=2, 6=1}
+			
+			/*
+			Stream<String> ohMy6 = Stream.of("lions", "tigers", "bears");
+			Map<Integer, Optional<Character>> map6 = ohMy6.collect(
+				Collectors.groupingBy(
+					String::length,
+					Collectors.mapping(s -> s.charAt(0),
+					Collectors.minBy(Comparator.naturalOrder()))));
+			System.out.println(map6); // {5=Optional[b], 6=Optional[t]}
+			
+			
+			Stream<String> ohMy7 = Stream.of("lions", "tigers", "bears");
+			Map<Integer, Optional<Character>> map7 = ohMy7.collect(
+				groupingBy(
+				String::length,
+				mapping(s -> s.charAt(0),
+				minBy(Comparator.naturalOrder()))));
+			System.out.println(map); // {5=Optional[b], 6=Optional[t]}
+			*/
+			
+			System.out.println();
+		}
 
 		public static void main(String...args){
 			lazilyStream();
+			collecting();
 			//threeDigitChaining();
+			collecting();
+			collectingIntoMaps();
+			collectingUsingGPM();
 			
 		}
 }
