409a08483438132ad3c45a80dbacbec54ea67789
Rafael <rafael_possenti@hotmail.com>
2017-08-21 12:40:24 -0300

    Runnable

--- Cap7/Creating a Thread/Exe.java
+++ Cap7/Creating a Thread/Exe.java
@@ -1,9 +1,16 @@
 public class Exe{
 	public static void main(String[] args) {
 		System.out.println("begin");
+		
 		(new ReadInventoryThread()).start();
 		(new Thread(new PrintData())).start();
 		(new ReadInventoryThread()).start();
+		 
+		/*** none will actually execute a task on a separate processing thread
+		new PrintData().run();
+		(new Thread(new PrintData())).run();
+		(new ReadInventoryThread()).run();
+		**/
 		System.out.println("end");
 	}
 }
