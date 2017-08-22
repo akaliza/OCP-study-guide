732b0d797ed57ee56f888458e6a78f118a427b0c
Rafael <rafael_possenti@hotmail.com>
2017-08-22 17:38:33 -0300

    Cap7

--- Cap7/Working with Parallel Streams/Parallel.java
+++ Cap7/Working with Parallel Streams/Parallel.java
@@ -16,5 +16,19 @@ public class Parallel {
 			Arrays.asList(1,2,3,4,5,6)
 				.parallelStream()
 				.forEach(s -> System.out.print(s+" "));
+			System.out.println();
+			
+			Arrays.asList("jackal","kangaroo","lemur")
+				.parallelStream()
+				.map(s -> s.toUpperCase())
+				.forEach(System.out::println);
+			System.out.println();
+
+			Arrays.asList("jackal","kangaroo","lemur")
+				.parallelStream()
+				.map(s -> {System.out.println(s); return s.toUpperCase();})
+				.forEach(System.out::println);
+
+					
 	}
 }
