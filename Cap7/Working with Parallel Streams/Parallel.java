be4993e9e854c00d8e64e59bbda8c345f359bf48
Rafael Possenti <rafael_possenti@hotmail.com>
2017-08-24 23:21:56 -0300

    IO

--- Cap7/Working with Parallel Streams/Parallel.java
+++ Cap7/Working with Parallel Streams/Parallel.java
@@ -4,6 +4,12 @@ import java.util.stream.*;
 public class Parallel {
 	
 	public static void main(String[] args) {
+											  
+			System.out.println(Arrays.asList(1,2,3,4,5,6)
+									.stream()
+									.reduce(0,(a,b) -> (a-b))); //1-2=-1;-1-3=-4;-4-4=-8;-8-5=-13;-13-6=-21
+			System.out.println();
+			
 			Stream<Integer> stream = Arrays.asList(1,2,3,4,5,6).stream();
 			Stream<Integer> parallelStream = stream.parallel();
 			Stream<Integer> parallelStream2 = Arrays.asList(1,2,3,4,5,6).parallelStream();
