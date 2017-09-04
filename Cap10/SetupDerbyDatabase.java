286e8bef784bd80d6fe85075d2d9235b1bd928d2
Rafael <rafael_possenti@hotmail.com>
2017-09-04 12:52:09 -0300

    remove column time

--- Cap10/SetupDerbyDatabase.java
+++ Cap10/SetupDerbyDatabase.java
@@ -11,15 +11,14 @@ public class SetupDerbyDatabase {
 			stmt.executeUpdate("CREATE TABLE animal ("
 				+ "id INTEGER PRIMARY KEY, "
 				+ "species_id integer, "
-				+ "name VARCHAR(255), "
-				+ "date_born TIMESTAMP)");
+				+ "name VARCHAR(255))");
 			stmt.executeUpdate("INSERT INTO species VALUES (1, 'African Elephant', 7.5)");
 			stmt.executeUpdate("INSERT INTO species VALUES (2, 'Zebra', 1.2)");
-			stmt.executeUpdate("INSERT INTO animal VALUES (1, 1, 'Elsa', '2001-05-06 02:15')");
-			stmt.executeUpdate("INSERT INTO animal VALUES (2, 2, 'Zelda', '2002-08-15 09:12')");
-			stmt.executeUpdate("INSERT INTO animal VALUES (3, 1, 'Ester', '2002-09-09 10:36')");
-			stmt.executeUpdate("INSERT INTO animal VALUES (4, 1, 'Eddie', '2010-06-08 01:24')");
-			stmt.executeUpdate("INSERT INTO animal VALUES (5, 2, 'Zoe', '2005-11-12 03:44')");
+			stmt.executeUpdate("INSERT INTO animal VALUES (1, 1, 'Elsa')");
+			stmt.executeUpdate("INSERT INTO animal VALUES (2, 2, 'Zelda')");
+			stmt.executeUpdate("INSERT INTO animal VALUES (3, 1, 'Ester')");
+			stmt.executeUpdate("INSERT INTO animal VALUES (4, 1, 'Eddie')");
+			stmt.executeUpdate("INSERT INTO animal VALUES (5, 2, 'Zoe')");
 		}
 	}
 }
