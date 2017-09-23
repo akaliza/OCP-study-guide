389a469fad42ed0ab2f6c49285591286d7705746
Rafael Possenti <rafael_possenti@hotmail.com>
2017-09-23 16:34:51 -0300

    Review Questions

--- Cap3/Review Questions/3/Q3.java
+++ Cap3/Review Questions/3/Q3.java
@@ -7,7 +7,7 @@ public class Q3 {
 		list.add("one");
 		list.add("two");
 		list.add(7);
-		for (String s: list) //erro aqui
+		for (String s: list) //error - can be Object [s: list]
 			System.out.print(s);
 	
 	}
--- Cap3/Review Questions/4/Q4.java
+++ Cap3/Review Questions/4/Q4.java
@@ -3,13 +3,13 @@ import java.util.function.*;
 
 public class Q4 {
 	public static void main(String...args){
-		ArrayDeque<String> greetings = new ArrayDeque<String>();
-		greetings.push("hello");
-		greetings.push("hi");
-		greetings.push("ola");
-		greetings.pop();
-		greetings.peek();
+		ArrayDeque<String> greetings = new ArrayDeque<String>(); 
+		greetings.push("hello"); //[hello]   push -> LIFO(last-in, first-out)
+		greetings.push("hi");	 //[hi,hello]
+		greetings.push("ola");	 //[ola,hi,hello]
+		greetings.pop();		 //[hi,hello]	
+		greetings.peek();		 //[hi,hello]
 		while (greetings.peek() != null)
-			System.out.print(greetings.pop());
+			System.out.print(greetings.pop()); //hihello
 	}
 }
