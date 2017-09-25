7236f1e94960a54c39d8b82e414d5e72dd9769ae
Rafael <rafael_possenti@hotmail.com>
2017-09-25 13:37:45 -0300

    BinarySearch

--- Cap3/Review Questions/15/Q15.java
+++ Cap3/Review Questions/15/Q15.java
@@ -10,3 +10,7 @@ public class Q15{
 		System.out.println(Collections.binarySearch(list, 1)); // D. The result is undefined.
 	}
 }
+
+/*The list is sorted in descending order. However, it is searched using the default order,
+which is sorted in ascending order. binarySearch() requires both to use the same sort
+order. Therefore, the precondition for binarySearch() is not met and the result is undefined.*/
