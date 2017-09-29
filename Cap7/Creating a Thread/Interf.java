d9dafc59749c2d4be3db591b5c692a0d65c07825
Rafael <rafael_possenti@hotmail.com>
2017-09-29 14:35:51 -0300

    Runnable

---
+++ Cap7/Creating a Thread/Interf.java
@@ -0,0 +1,10 @@
+public class Interf implements Runnable{
+	
+	public void run() {
+		System.out.println("Printing zoo inventory");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("teste");
+	}
+}
