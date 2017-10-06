8506e498b37bb783cd9967b2199b77ce33d67cee
Rafael Possenti <rafael_possenti@hotmail.com>
2017-10-05 23:27:16 -0300

    fork/join

--- Cap7/Managing Concurrent Processes/WeighAnimalAction.java
+++ Cap7/Managing Concurrent Processes/WeighAnimalAction.java
@@ -1,5 +1,13 @@
 import java.util.*;
 import java.util.concurrent.*;
+
+/*
+RecursiveAction is a task that can run in a ForkJoinPool; its compute() method
+performs the actual computation steps in the task. It is similar to RecursiveTask, but
+does not return a value.
+*/
+
+
 public class WeighAnimalAction extends RecursiveAction {
 	private int start;
 	private int end;
@@ -20,8 +28,10 @@ public class WeighAnimalAction extends RecursiveAction {
 			}
 		else {
 			int middle = start+((end-start)/2); //1°=5 2°=2 3°=7  
-			System.out.println("[start="+start+",middle="+middle+",end="+end+"]"); // 0 5 10 | 0 2 5 | 5 7 10    
-			invokeAll(new WeighAnimalAction(weights,start,middle), // x,0,5
+			System.out.println("[start="+start+",middle="+middle+",end="+end+"]"); // 0 5 10 | 0 2 5 | 5 7 10 
+
+			//Executes all the given tasks and returns a list of future objects when all the tasks are completed.			
+			invokeAll(new WeighAnimalAction(weights,start,middle), // x,0,5 
 					  new WeighAnimalAction(weights,middle,end));  // x,5,10		
 		}
 	}
@@ -29,7 +39,7 @@ public class WeighAnimalAction extends RecursiveAction {
 	public static void main(String[] args) {
 		Double[] weights = new Double[10];
 		ForkJoinTask<?> task = new WeighAnimalAction(weights,0,weights.length);
-		ForkJoinPool pool = new ForkJoinPool();
+		ForkJoinPool pool = new ForkJoinPool(); 
 		pool.invoke(task);
 		// Print results
 		System.out.println();
--- Cap7/Managing Concurrent Processes/WeighAnimalTask.java
+++ Cap7/Managing Concurrent Processes/WeighAnimalTask.java
@@ -33,8 +33,8 @@ public class WeighAnimalTask extends RecursiveTask<Double> {
 	public static void main(String[] args) {
 		Double[] weights = new Double[10];
 		ForkJoinTask<Double> task = new WeighAnimalTask(weights , 0 , weights.length);
-		ForkJoinPool  pool = new ForkJoinPool();
-		Double sum = pool.invoke(task);
+		ForkJoinPool  pool = new ForkJoinPool(); //is a thread pool for running fork/join tasks and it executes an instance of ForkJoinTask
+		Double sum = pool.invoke(task); //Executes the given task and returns the computed result.
 		System.out.println("Sum: "+sum);	
 	}
 	
