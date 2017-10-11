89316e917a1af21cfdda5cea1e065210e3c1733d
Rafael <rafael_possenti@hotmail.com>
2017-10-11 12:51:14 -0300

    DeadLock

--- Cap7/Review Questions/QT.java
+++ Cap7/Review Questions/QT.java
@@ -93,8 +93,28 @@ public class QT{
 			 Integer::sum));
 	}
 	
+	private static void q11() throws InterruptedException,ExecutionException{
+		Object o1 = new Object();
+		Object o2 = new Object();
+		
+		ExecutorService service = Executors.newFixedThreadPool(2);
+		Future<?> f1 = service.submit(() -> {
+			synchronized (o1) {
+				synchronized (o2) { System.out.println("Tortoise"); } // t1
+			}
+		});
+		
+		Future<?> f2 = service.submit(() -> {
+			synchronized (o2) {
+				synchronized (o1) { System.out.println("Hare"); } // t2
+			}
+		});
+		f1.get();
+		f2.get();
+	} 
+	
 	public static void main(String...args) throws Exception{
-		q10();
+		q11();
 	}
 }
 
