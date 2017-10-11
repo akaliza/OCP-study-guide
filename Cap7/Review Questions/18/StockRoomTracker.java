6434f848903055de09f4045b9fc952fafad2f42a
Rafael <rafael_possenti@hotmail.com>
2017-10-11 15:47:39 -0300

    CyclicBarrier

---
+++ Cap7/Review Questions/18/StockRoomTracker.java
@@ -0,0 +1,25 @@
+import java.util.concurrent.*;
+import java.util.stream.*;
+
+public class StockRoomTracker {
+	
+	public static void await(CyclicBarrier cb) { // j1
+		try { 
+			cb.await();  
+		} catch (InterruptedException | BrokenBarrierException e) {
+			// Handle exception
+		}
+	}
+	/*
+	even if the limit(9) statement was changed to limit(10), the program could still hang, 
+	since the JVM might not allocate 10 threads to the parallel stream.
+	*/
+	public static void main(String[] args) {
+		CyclicBarrier cb = new CyclicBarrier(10,() -> System.out.println("Stock Room Full!")); // j2
+		
+		IntStream.iterate(1, i -> 1)
+			.limit(9)
+			.parallel()
+			.forEach(i -> await(cb)); // j3 
+	}
+}
