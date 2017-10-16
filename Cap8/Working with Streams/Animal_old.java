dbffdeabfbe5af2558cde1c1984151830e483afd
Rafael <rafael_possenti@hotmail.com>
2017-10-16 13:19:32 -0200

    Serializable

--- Cap8/Working with Streams/Animal_old.java
+++ Cap8/Working with Streams/Animal_old.java
@@ -1,19 +1,10 @@
 import java.io.Serializable;
 public class Animal implements Serializable {
-	private static final long serialVersionUID = 2L;
-	private transient String name;
-	private transient int age = 10;
-	private static char type = 'C';
+	private static final long serialVersionUID = 1L;
+	private String name;
+	private int age;
+	private char type;
 	
-	{this.age = 14;}
-	
-	public Animal() {
-		this.name = "Unknown";
-		this.age = 12;
-		this.type = 'Q';
-	}
-
-
 	public Animal(String name, int age, char type) {
 		this.name = name;
 		this.age = age;
