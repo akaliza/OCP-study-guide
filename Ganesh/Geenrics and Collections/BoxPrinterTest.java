7e97be615822e17ef641682d84f92f6d75805d61
Rafael <rafael_possenti@hotmail.com>
2017-11-01 11:26:08 -0200

    Generics

---
+++ Ganesh/Geenrics and Collections/BoxPrinterTest.java
@@ -0,0 +1,19 @@
+// This program shows container implementation using generics
+class BoxPrinter<T> {
+	private T val;
+	public BoxPrinter(T arg) {
+		val = arg;
+	}
+	public String toString() {
+		return "[" + val + "]";
+	}
+}
+
+public class BoxPrinterTest {
+	public static void main(String []args) {
+		BoxPrinter<Integer> value1 = new BoxPrinter<>(new Integer(10));
+		System.out.println(value1);
+		BoxPrinter<String> value2 = new BoxPrinter<>("Hello world");
+		System.out.println(value2);
+	}
+}
