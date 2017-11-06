a1046ec4b9339f3ba76a3f82fdbed698a93d6c7c
Rafael <rafael_possenti@hotmail.com>
2017-11-06 16:57:06 -0200

    RQ part 2

---
+++ Ganesh/Geenrics and Collections/QuestionTime/DequeTest.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class DequeTest {
+	public static void main(String []args) {
+		Deque<Integer> deque = new ArrayDeque<>();
+		deque.addAll(Arrays.asList(1, 2, 3, 4, 5));
+		System.out.println("The removed element is: " + deque.remove());
+		// ERROR?
+	}
+}
+
--- Ganesh/Geenrics and Collections/QuestionTime/DoubleUse.java
+++ Ganesh/Geenrics and Collections/QuestionTime/DoubleUse.java
@@ -3,6 +3,7 @@ import java.util.stream.DoubleStream;
 public class DoubleUse {
 	public static void main(String []args) {
 		DoubleStream nums = DoubleStream.of(1.0, 2.0, 3.0).map(i -> -i); // #1
-		System.out.printf("count = %d, sum = %f", nums.count(), nums.sum());
+		System.out.printf("count = %d, sum = %f", nums.count(), nums.sum()); 
+		/*D. T his program crashes by throwing a java.lang.IllegalStateException*/
 	}
 }
