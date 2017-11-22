800836c762492dc7dd2176aaccf6ee207724a946
Rafael <rafael_possenti@hotmail.com>
2017-11-22 18:04:11 -0200

    Mock until 30

--- Ganesh/Mock/MockTest.java
+++ Ganesh/Mock/MockTest.java
@@ -2,13 +2,14 @@ import java.util.stream.Stream;
 import java.util.Comparator; 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.*;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
-
+import java.util.function.*;
 
 class MockTest {
 	public static void main(String []args) {
-		  question24(); 
+		  question27(); 
 	}
 	
 	public static void question1(){
@@ -25,18 +26,33 @@ class MockTest {
 		Arrays.sort(brics, null);
 		Arrays.stream(brics).forEach(country -> System.out.print(country + " ")); // Brazil, China, India, Russia
 	}
-	//10/Jan/15
+	
 	public static void question24(){
 		DateTimeFormatter fromDateFormat = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 		LocalDate firstOct2015 = LocalDate.parse("01/10/2015",fromDateFormat);
 		DateTimeFormatter toDateFormat = DateTimeFormatter.ofPattern("dd/MMM/YY");
 		System.out.println(firstOct2015.format(toDateFormat));
 	}
+	
+	public static void question26(){
+		IntFunction<UnaryOperator<Integer>> func = i -> j -> i * j;
+		Integer apply = func.apply(10).apply(20);
+		System.out.println(apply);
+	}	
+	
+	public static void question27(){
+		List<Map<List<Integer>, List<String>>> list = new ArrayList<>(); // ADD_MAP
+		Map<List<Integer>, List<String>> map = new HashMap<>();
+		list.add(null); // ADD_NULL
+		list.add(map);
+		list.add(new HashMap<List<Integer>, List<String>>()); // ADD_HASHMAP
+		list.forEach(e -> System.out.print(e + " ")); // ITERATE
+		
+	}
+	
 	/*
-	a) DateTimeFormatter firstOct2015 = DateTimeFormatter.parse("01/10/2015", fromDateFormat);
-	b) LocalTime firstOct2015 = LocalTime.parse("01/10/2015",fromDateFormat);
-	c) Period firstOct2015 = Period.parse("01/10/2015", fromDateFormat);
-	*/
+	e) When run, this program will print the following: null {} {}
 
 	
+	*/
 }
