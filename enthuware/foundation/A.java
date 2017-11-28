10744420d5e96bf0133f28eb10d45a6822a1a123
Rafael <rafael_possenti@hotmail.com>
2017-11-28 13:25:27 -0200

    Enthuware

--- enthuware/foundation/A.java
+++ enthuware/foundation/A.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class A  // outer class
 {
    static public class B //Static Nested class . It can be used in other places: A.B b = new A.B(); There is no outer instance.
@@ -6,11 +8,16 @@ public class A  // outer class
    
    class C //Inner class. It can only be used like this: A.C c = new A().new C(); Outer instance is needed.
    {
+	   private final static int x = 10;
+	   public void showX(){
+		   System.out.println(x);
+	   }
    }
    
    public static void main(String...args){
 		A.B b = new A.B();
 		A.C c = new A().new C();
+		c.showX();
    }
    
 }
