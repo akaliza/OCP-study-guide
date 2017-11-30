5cf83786bd940f143d7e425f8ecdd4622312e5e3
Rafael <rafael_possenti@hotmail.com>
2017-11-30 11:34:39 -0200

    Whizlabs Diagnostic Test

---
+++ whizlabs/Diagnostic Test/Whiz18.java
@@ -0,0 +1,13 @@
+import java.util.List; 
+import java.util.ArrayList; 
+
+public class Whiz18{	
+	public static void main(String[] args){
+		List<String> lst = new ArrayList<>(); 
+		lst.add("a");
+		lst.add("ab");
+		lst.stream().forEach(s -> System.out.print(s.length())).forEach(System.out::print); //doesn't compile
+		//lst.forEach(System.out::print); compile
+	}
+	
+}
