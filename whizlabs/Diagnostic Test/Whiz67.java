727ccaef29bbc6090fa51e2b90ab52920436c62d
Rafael Possenti <rafael_possenti@hotmail.com>
2018-01-01 18:51:11 -0200

    Diagnostic Test - Review

--- whizlabs/Diagnostic Test/Whiz67.java
+++ whizlabs/Diagnostic Test/Whiz67.java
@@ -3,7 +3,8 @@ import java.util.function.*;
 
 public class Whiz67{
 	public static void main(String[] args){
-		BiFunction<Double,Double,String> fun = (d,i) -> d + i.toString();
+		BiFunction<Double,Double,String> fun = (d1,d2) -> d1 + d2.toString();
 		System.out.println(fun.apply(3.5,2.5));	
+		
 	}
 }
