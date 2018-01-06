778aa969467112e9d019522a2db86d491ef67f8f
Rafael Possenti <rafael_possenti@hotmail.com>
2018-01-06 19:10:28 -0200

    Practice Test III - 20

---
+++ whizlabs/Practice Test III/Whiz1.java
@@ -0,0 +1,9 @@
+public class Whiz1{
+	public static void main(String[] args){
+		Outer.Inner oi = new Outer.Inner();		
+	}
+}	
+class Outer{
+	static class Inner{		
+	}
+}
---
+++ whizlabs/Practice Test III/Whiz13.java
@@ -0,0 +1,6 @@
+public class Whiz13{
+	public static void main(String[] args){
+		if(args != null)
+		   System.out.println(args); //Ljava.lang.String;@15db9742		
+	}
+}	
